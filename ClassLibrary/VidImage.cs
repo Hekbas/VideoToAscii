9ffe04305389662b83a6f1422405c48541552803
Hekbas <hekbas@gmail.com>
2021-04-17 14:53:24 +0200

    Agregar archivos de proyecto.

---
+++ ClassLibrary/VidImage.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading;
+using Emgu.CV;
+
+namespace ClassLibrary
+{
+    public class VidImage
+    {
+        private string[] asciiString = new string[400];
+
+        private Image vidImage;
+        private FrameDimension dimension;
+        private int numFrames;
+        private int currentFrame;
+
+        public VidImage()
+        {
+            vidImage = Image.FromFile(@"..\..\..\..\Repos\VideoSource\BadApple.gif");
+            dimension = new FrameDimension(vidImage.FrameDimensionsList[0]);
+            numFrames = 3000;
+        }
+
+        public VidImage(int currentFrame)
+        {
+            vidImage = Image.FromFile(@"..\..\..\..\Repos\VideoSource\BadApple.gif");
+            dimension = new FrameDimension(vidImage.FrameDimensionsList[0]);
+            numFrames = vidImage.GetFrameCount(dimension);
+            this.currentFrame = currentFrame;
+        }
+
+      
+        //GET & SETS
+        public string[] GetAsciiString()
+        {
+            return this.asciiString;
+        }
+        public void SetAsciiString(string[] asciiString)
+        {
+            this.asciiString = asciiString;
+        }
+
+        public int GetNumFrames()
+        {
+            return this.numFrames;
+        }
+
+        public int GetCurrentFrame()
+        {
+            return this.currentFrame;
+        }
+        public void SetCurrentFrame(int currentFrame)
+        {
+            this.currentFrame = currentFrame;
+        }
+
+
+        //METHODS
+        public void ThreadCallBack(object newBactchAmmount)
+        {
+            int batchAmmount = (int)newBactchAmmount;
+
+            for (int i = 0; i < batchAmmount; i++)
+            {
+                asciiString[i] = GrayscaleImageToASCII(NextFrame());
+
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"thread finished!");
+        }
+
+        public Image NextFrame()
+        {
+            currentFrame++;
+            vidImage.SelectActiveFrame(dimension, currentFrame); //find the frame
+            return (Image)vidImage.Clone(); //return a copy of it
+        }
+
+        public string GrayscaleImageToASCII(Image img)
+        {
+            StringBuilder asciiString = new StringBuilder();
+            Bitmap bmp = null;
+            Size size = new Size(170, 64);
+
+            // Create a bitmap from the image
+
+            bmp = new Bitmap(img, size);
+
+            // Loop through each pixel in the bitmap
+
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color col = bmp.GetPixel(x, y);
+
+                    int gValue = col.R; //between 0-255.
+
+                    // Append the "color" using various darknesses of ASCII character.
+
+                    asciiString.Append(GetGrayShade(gValue));
+
+                    // If we're at the width, insert a line break
+
+                    if (x == bmp.Width - 1)
+                        asciiString.Append("\n");
+                }
+            }
+            return asciiString.ToString();
+        }   
+
+        public string GetGrayShade(int color)
+        {
+            string asciival = " ";
+
+            //BLACK
+            if (color <= 50) { asciival = " "; } 
+
+            else if (color <= 70) { asciival = "."; }
+
+            else if (color <= 100) { asciival = "*"; }
+
+            else if (color <= 130) { asciival = ":"; }
+
+            else if (color <= 160) { asciival = "o"; }
+
+            else if (color <= 180) { asciival = "&"; }
+
+            else if (color <= 200) { asciival = "8"; }
+
+            else if (color <= 230) { asciival = "#"; }
+
+            else { asciival = "@"; }
+            //WHITE
+
+            return asciival;
+        }
+
+        public void SaveAscii(string path, List<VidImage> vidImageList)
+        {
+            StreamWriter w = new StreamWriter(path);
+
+            for (int i = 0; i < vidImageList.Count; i++)
+            {
+                string[] temp = vidImageList[i].GetAsciiString();
+
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    Console.SetCursorPosition(0, 0);
+                    w.WriteLine(temp[j]);
+                    Thread.Sleep(30);
+                }
+            }
+            w.Close();
+        }
+
+        public void LoadAscii(string path, VidImage vidImage)
+        {
+            StreamReader rLine = new StreamReader(path);
+            string[] ascii = new string[GetNumFrames()];
+            int lines = 0;
+
+            while (rLine.ReadLine() != null)
+            {
+                for (int i = 0; i < 64; i++)
+                {
+                    ascii[lines] += rLine.ReadLine();
+                    ascii[lines] += "\n";
+                }
+                lines++;
+            }
+            rLine.Close();
+
+            for (int i = 0; i < GetNumFrames(); i++)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(ascii[i]);
+                Thread.Sleep(28);
+            }
+        }
+    }
+}
---
+++ VideoToAscii/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading;
+using ClassLibrary;
+
+namespace VideoToAscii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo op;
+            Console.CursorVisible = false;
+
+            Program program = new Program();
+            VidImage vidInfo = new VidImage();
+            List<VidImage> vidImageList = new List<VidImage>();
+                      
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("MENU PRINCIPAL\n" +
+                    "1 - Convert Video to Ascii (Save to .txt file)\n" +
+                    "2 - Play Ascii from array\n" +
+                    "3 - Play Ascii from .txt");
+
+                op = Console.ReadKey(true);
+
+                if (op.Key == ConsoleKey.D1)
+                {
+                    Console.Clear();
+                    //Convert vid to ascii
+                    program.ConvertVidToAscii(vidInfo, vidImageList);
+
+                    Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("Saving video...");
+
+                    //Save asci into .txt
+                    vidInfo.SaveAscii(@"..\..\..\..\Repos\SavedAscii\Ascii.txt", vidImageList);
+
+                    Console.WriteLine("Video to Ascii succesful!");
+                    Console.ReadLine();
+                }
+                else if (op.Key == ConsoleKey.D2)
+                {
+                    //Get ascii from .txt and play it
+                    program.PlayAsciiFromFile(vidImageList);
+                }
+                else if (op.Key == ConsoleKey.D3)
+                {
+                    vidInfo.LoadAscii(@"..\..\..\..\Repos\SavedAscii\Ascii.txt", vidInfo);
+                    Console.WriteLine("Reached end of video");
+                    Console.ReadLine();
+                }
+                else if (op.Key == ConsoleKey.Escape)
+                {
+                    Environment.Exit(0);
+                }
+            } while (op.Key != ConsoleKey.Escape);
+          
+            Console.ReadLine();
+            Console.Clear();
+
+            //CARGAR ASCII DEL .TXT A CONSOLE
+            //VidImage.LoadAscii(@"C:\Users\hekba\Downloads\Ascii.txt", vidImage);
+        }
+
+        public void ConvertVidToAscii(VidImage vidInfo, List<VidImage> vidImageList)
+        {
+            int batchAmmount = 400;
+            int numThreads;
+            int numFrames = vidInfo.GetNumFrames();
+
+            //CONVERT VID TO ASCII
+            if (numFrames % 400 == 0)
+                numThreads = numFrames / 400;
+            else
+                numThreads = numFrames / 400 + 1;
+
+            numThreads = 4;
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                int newBatchAmmount;
+
+                VidImage tmp = new VidImage(batchAmmount * i);
+                
+                if (i == numThreads-1)
+                {
+                    newBatchAmmount = numFrames - i * batchAmmount;
+                }
+                else
+                {
+                    newBatchAmmount = batchAmmount;
+                }
+
+                vidImageList.Add(tmp);
+                ThreadPool.QueueUserWorkItem(tmp.ThreadCallBack, newBatchAmmount);
+                Console.WriteLine($"Threads created: {i+1}");
+            }
+            //WaitHandle.WaitAll(doneEvents);
+        }
+
+        public void PlayAsciiFromFile(List<VidImage> vidImageList)
+        {
+            for (int i = 0; i < vidImageList.Count; i++)
+            {
+                string[] temp = vidImageList[i].GetAsciiString();
+
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    Console.SetCursorPosition(0, 0);
+                    Console.WriteLine(temp[j]);
+                    Thread.Sleep(30);
+                }
+            }
+        }
+    }
+}
